a58a3606a88a07a5c2e915776e1d4540235c7275
Miguel Cutri <vitor.cutri@hotmail.com>
2024-06-19 17:28:25 -0300

    feat: basic structure

--- Ignite-timer/src/pages/Home/Home.tsx
+++ Ignite-timer/src/pages/Home/Home.tsx
@@ -1,31 +1,37 @@
 import { Play } from 'phosphor-react'
+import {
+  CountdownContainer,
+  FormContainer,
+  HomeContainer,
+  Separator,
+} from './styles'
 
 export default function Home() {
   return (
-    <div>
+    <HomeContainer>
       <form action="">
-        <div>
+        <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
           <input id="task" />
 
           <label htmlFor="minutesAmount">durante</label>
           <input id="minutesAmount" type="number" />
           <span>minutos.</span>
-        </div>
+        </FormContainer>
 
-        <div>
+        <CountdownContainer>
           <span>0</span>
           <span>0</span>
-          <span>:</span>
+          <Separator>:</Separator>
           <span>0</span>
           <span>0</span>
-        </div>
+        </CountdownContainer>
 
         <button type="submit">
           <Play size={24} />
           Começar
         </button>
       </form>
-    </div>
+    </HomeContainer>
   )
 }
